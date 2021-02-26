124c44fd78fcf47590d229dac302d95cf5626dc4
Arthur Lozano <chillout885@gmail.com>
2021-02-26 00:08:46 -0800

    JSDOC

--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
 'use strict';
 
+/** 
+ *  @fileOverview App is what is needed with require modules into the server.  Routes are created, middleware is going out grabbing data and bringing it back beforing going into the request.  At the bottom exported an object with express app with a seperate method that can start the actual server
+ *
+ *  @author        Ruben Arthur Lozano jR
+ *
+ *
+ *  @requires     NPM:npm_module_1
+ *  @requires     LAB 02
+ *  @requires     https://basic-express-server-lab2.herokuapp.com/
+ */
 
 // 3rd Party Dependencies (modules)
 
@@ -20,7 +30,7 @@ const logger = require('./middleware/logger.js');
 //Our Own Global Middlewear
 app.use(logger);
 
-//Route-Possible middleware in the route to help with code review questions
+//Middleware 
 app.get('/person', validator, (req, res) => {
   // console.log('Test');
   res.status(200).json({
